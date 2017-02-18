d01748d26b74a685c414cffe4fe87e2cee17b98a
gonggf <842012173@qq.com>
2017-02-18 14:35:38 +0800

    update SeleniumDriver

---
+++ src/main/java/atuoframe/SeleniumDriver.java
@@ -0,0 +1,11 @@
+package atuoframe;
+
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by Thinkpad on 2017/2/18.
+ */
+public class SeleniumDriver {
+    public static WebDriver driver;
+
+}
